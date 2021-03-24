0589e5dce71afa019c6a7e9182d351bbfff6fc2f
Juan Gaytan <arc_juan_1@hotmail.com>
2021-03-23 19:10:26 -0600

    Se agregan todos los componentes necesarios de la app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react'
+import Clima from './components/Clima';
+import Formulario from './components/Formulario';
+import Header from './components/Header';
 
 function App() {
+
+    // state del formulario
+    const [busqueda, guardarBusqueda] = useState({
+      ciudad: "",
+      pais: ""
+  });
+    const [consultar, guardarConsultar] = useState(false);
+    const [resultado, guardarResultado] = useState({});
+
+  //Extraer ciudad y pais
+  const {ciudad, pais} = busqueda;
+
+  useEffect(() =>{
+      const consultarAPI = async () =>{
+
+        if(consultar){
+          const appId = "fafc4dae62cfff72b0932125ba1298b4";
+          const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+          guardarResultado(resultado);
+        }
+      }
+
+      consultarAPI();
+  }, [consultar]);
+
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+        <Header
+          titulo = "Clima React App"
+        />
+          <div className="contenedor-form">
+            <div className="container">
+              <div className="row">
+                <div className="col m6 s12">
+                  <Formulario 
+                    busqueda = {busqueda}
+                    guardarBusqueda = {guardarBusqueda}
+                    guardarConsultar = {guardarConsultar}
+                  />
+                </div>
+                <div className="col m6 s12">
+                  <Clima />
+                </div>
+              </div>
+            </div>
+          </div>
+    </Fragment>
   );
 }
 
